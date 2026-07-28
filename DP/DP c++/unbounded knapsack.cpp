1736cce006c9c61d34861e21390dd77423d711a5
madhav joshi <150261093+madha-v@users.noreply.github.com>
2026-07-28 16:13:05 +0530

    Update unbounded knapsack.cpp

--- DP/DP c++/unbounded knapsack.cpp
+++ DP/DP c++/unbounded knapsack.cpp
@@ -30,3 +30,49 @@ int unbounded(vector<int>&val , vector<int>&wt , int n, int w)
     int w=7;
     cout<<unbounded(val ,wt , n, w);
  }
+// TOP-DOWN APPROACH
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int dp[1001][1001];
+
+int solve(vector<int>& wt, vector<int>& val, int i, int W, int n)
+{
+    // Base case
+    if(i == n || W == 0)
+        return 0;
+
+    if(dp[i][W] != -1)
+        return dp[i][W];
+
+    // Skip current item
+    int skip = solve(wt, val, i + 1, W, n);
+
+    // Take current item (stay at same index)
+    int take = 0;
+    if(wt[i] <= W)
+    {
+        take = val[i] + solve(wt, val, i, W - wt[i], n);
+    }
+
+    return dp[i][W] = max(take, skip);
+}
+
+int unboundedKnapsack(int n, int W, vector<int>& val, vector<int>& wt)
+{
+    memset(dp, -1, sizeof(dp));
+    return solve(wt, val, 0, W, n);
+}
+
+int main()
+{
+    vector<int> val = {10, 40, 50, 70};
+    vector<int> wt = {1, 3, 4, 5};
+    int W = 8;
+
+    cout << unboundedKnapsack(val.size(), W, val, wt);
+
+    return 0;
+}
